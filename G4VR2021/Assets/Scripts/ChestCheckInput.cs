14729fe58b32d337f57ed73d87a9cf163f67bb60
PetrosIsaakidis <p.isaakidis@campus.lmu.de>
2021-07-29 10:06:13 +0200

    Chest_done

---
+++ G4VR2021/Assets/Scripts/ChestCheckInput.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ChestCheckInput : MonoBehaviour
+{
+    Text displayInput;
+    public Text txt;
+    public int pin = 1234;
+    public string input;
+    private bool pinWasGuessed;
+    //public GameObject DoorLeft;
+    //public GameObject DoorRight;
+
+    enum LidState
+    {
+        Closed,
+        Opening,
+        Closing,
+        Open
+    }
+
+    LidState chestState = LidState.Closed;
+
+    public Transform hinge;
+
+    public float openSpeed = 1f;
+    //SerializeField startAngle;
+    //SerializeField endAngle;
+
+    public float startAngle = 0f;
+    public float endAngle = -40f;
+
+
+    public void OpenChest()
+    {
+        if (chestState == LidState.Closed)
+            StartCoroutine(ToggleChest(startAngle, endAngle, LidState.Opening, LidState.Open));
+    }
+
+    public void CloseChest()
+    {
+        if (chestState == LidState.Closed)
+            StartCoroutine(ToggleChest(endAngle, startAngle, LidState.Opening, LidState.Open));
+    }
+
+    IEnumerator ToggleChest(float angleStart, float angleEnd, LidState initial, LidState complete)
+    {
+        chestState = initial;
+
+        float time = 0f;
+        while (time <= 1f)
+        {
+            // Calculate currrent angle
+            float angle = Mathf.Lerp(angleStart, angleEnd, time);
+
+            //Get a copy of current Euler angles
+            Vector3 euler = hinge.eulerAngles;
+
+            // Set current angle
+            euler.x = angle;
+
+            // Reapply Euler angles to transform
+            hinge.eulerAngles = euler;
+
+            // Increment time
+            time = time + Time.deltaTime / openSpeed;
+
+            yield return null;
+
+        }
+
+        chestState = complete;
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayInput = GetComponent<Text>();
+        pinWasGuessed = false;
+    }
+
+    public void _addNumber(string num)
+    {
+        if (!pinWasGuessed)
+        {
+            input = input + num;
+            displayInput.text = input;
+            checkIfInputLenghtIs4();
+        }
+    }
+
+    private void checkIfInputLenghtIs4()
+    {
+        if (input.Length < 4)
+        {
+            return;
+        }
+
+        else if (input.Length == 4)
+        {
+            //check if pin is correct
+            int enteredPin = Int32.Parse(input);
+            if (enteredPin == pin)
+            {
+                txt.GetComponent<Text>().text = "Pin is correct!";
+                pinWasGuessed = true;
+                //DoorLeft.GetComponent<OpenLiftDoor>()._openDoor();
+                //DoorRight.GetComponent<OpenLiftDoor>()._openDoor();
+
+                OpenChest();
+                return;
+            }
+            else
+            {
+                txt.GetComponent<Text>().text = "Pin is incorrect! Try again...";
+                input = "";
+                displayInput.text = input;
+            }
+        }
+    }
+}
