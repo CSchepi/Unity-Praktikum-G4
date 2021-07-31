7c71cd897d6c0d313b549221bdde84be5c3ab736
yaraaaa <Y.Fanger@campus.lmu.de>
2021-07-31 16:59:49 +0200

    Drill works and reposition key

--- G4VR2021/Assets/Scripts/VaultUnlockDrill.cs
+++ G4VR2021/Assets/Scripts/VaultUnlockDrill.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class VaultUnlockDrill : MonoBehaviour
 {
+    public float startAngle = -1.41f;
+    public float endAngle = 160f;
+    public float openSpeed;
+    public Transform hinge;
 
     void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject.name);
-        if (other.gameObject.name == "Drill")
+        if (other.tag == "Drill")
         {
+            Debug.Log("Open VAULT");
             Debug.Log(other.gameObject.name);
-            var rb = GameObject.Find("vault_door").GetComponent<Rigidbody>();
-            rb.isKinematic = false;
+            //var rb = GameObject.Find("vault_door").GetComponent<Rigidbody>();
+            //rb.isKinematic = false;
+            ToggleChest(startAngle, endAngle);
+            this.gameObject.SetActive(false);
         }
+    }
+
+    IEnumerator ToggleChest(float angleStart, float angleEnd)
+    {
+        //audio.Play();
+
+        float time = 0f;
+        while (time <= 1f)
+        {
+            // Calculate currrent angle
+            float angle = Mathf.Lerp(angleStart, angleEnd, time);
+
+            //Get a copy of current Euler angles
+            Vector3 euler = hinge.eulerAngles;
+
+            // Set current angle
+            euler.y = angle;
+
+            // Reapply Euler angles to transform
+            hinge.eulerAngles = euler;
+
+            // Increment time
+            time = time + Time.deltaTime / openSpeed;
+
+            yield return null;
+
+        }
+
     }
 }
